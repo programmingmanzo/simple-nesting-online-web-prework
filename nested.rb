b2635e1833d45bcffc3b344a4ed0b92a51226e53
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-10 15:04:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages.to_s]
+     convert_to_string = programmer_hash[:dennis_ritchie][:languages]
+     convert_to_string.to_s 
 end
 
 def adding_matz
