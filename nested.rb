2ac1d8481bece2a75ead63ce519c1f75e077e443
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-10 15:25:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,7 +129,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages[1]] = "Assembly"
+     adding_string = programmer_hash[:dennis_ritchie][:languages]
+     adding_string[1] = "Assembly"
      programmer_hash
 
 end
